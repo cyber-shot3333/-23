e7c64f81ea5c7c0d8be3b7a9023d3d8742e8d8f4
Илья Смирнов <cyber-shot3333@yandex.ru>
2022-01-21 15:45:07 +0300

    Добавьте файлы проекта.

---
+++ Задание 23/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_23
+{
+    class Program
+    {
+        static int n;
+
+        static void Main(string[] args)
+        {           
+            try
+            {
+                Console.Write("Введите целое число, факториал которого хотите получить: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n >= 0)
+                {
+                    FactorialAsync();
+                }
+                else
+                {
+                    Console.WriteLine("Число не должно быть отрицательным");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }         
+            Console.ReadKey();
+        }
+        static void Factorial()
+        {
+            ulong result = 1;
+            if (n >= 2)
+            {
+                for (int i = 1; i <= (n - 1); i++)
+                {
+                    result *= (ulong)(i + 1);
+                }
+            }
+            Console.WriteLine($"{n}! = {result}");
+        }
+        static async void FactorialAsync()
+        {           
+            await Task.Run(() => Factorial());
+        }
+    }
+}
